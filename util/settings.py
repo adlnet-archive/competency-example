ecc7d93a6c259765b2569986f7f7e20059402bed
tom creighton <tjcreighton@gmail.com>
2014-01-09 11:16:41 -0500

    nothing really

--- util/settings.py
+++ util/settings.py
@@ -1,6 +1,7 @@
 import base64
 
 LRS_STATEMENT_ENDPOINT = 'https://lrs.adlnet.gov/xapi/statements'
+# LRS_STATEMENT_ENDPOINT = 'http://localhost:8000/xapi/statements'
 
 ENDPOINT_AUTH_USERNAME = 'tom'
 
